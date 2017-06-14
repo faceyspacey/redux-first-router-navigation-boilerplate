bc3eb146c904446aac142f240847e2715237e137
James Gillmore <james@faceyspacey.com>
2017-06-14 13:16:05 -0700

    fix($qa): insuring it works for anyone that wants to try it out

--- README.md
+++ README.md
@@ -10,14 +10,21 @@ https://github.com/jhen0409/react-native-debugger
 git clone https://github.com/faceyspacey/redux-first-router-navigation-boilerplate
 cd redux-first-router-navigation-boilerplate
 yarn
+react-native run-ios
+
+#once the simulator opens and the app is built, kill the packager terminal
+
 yarn start
+
+#React Native Debugger will now open while the app starts.
+#If you keep the simulator + debugger open, you can just do yarn start anytime you want to play with it again
 ```
 
 ## What To Do
 - examine the components (particularly Navigators: Stack.js, Tabs.js, Drawer.js, and secondly Screens: RecentChats, Notfications, Chats. In that order.)
 - examine the reducers
+- examine index.ios.js
 - examine src/index.js
 - examine src/configureStore.js
-- examine index.ios.js
 
 At one point I got it working with deep linking in Android, but haven't visited that in a while. So assume it's iOS only.
--- src/configureStore.js
+++ src/configureStore.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import { connectRoutes, redirect } from 'redux-first-router'
 // import reduxNavigation from 'redux-first-router-navigation'
-import reduxNavigation from '../../redux-first-router-navigation/src/index.js'
+import reduxNavigation from 'redux-first-router-navigation'
 import createReducer from './reducers'
 import { Stack } from './components/Stack'
 import { Tabs, TabsStack } from './components/Tabs'
@@ -12,28 +12,35 @@ import { Drawer, DrawerStack } from './components/Drawer'
 
 export default history => {
   const initialPath = history.entries[0].pathname
-  const { reducer, middleware, enhancer } = connectRoutes(history, {
-    HOME: { path: '/tabs/recent', navKey: 'home' },
-    CONTACTS: { path: '/tabs/contacts', navKey: 'contacts' },
-    NOTIFICATIONS: { path: '/tabs/notifications', navKey: 'tabsStack' },
-    CHAT: { path: '/tabs/chat/:user', navKey: 'tabsStack' },
+  const { reducer, middleware, enhancer } = connectRoutes(
+    history,
+    {
+      HOME: { path: '/tabs/recent', navKey: 'home' },
+      CONTACTS: { path: '/tabs/contacts', navKey: 'contacts' },
+      NOTIFICATIONS: { path: '/tabs/notifications', navKey: 'tabsStack' },
+      CHAT: { path: '/tabs/chat/:user', navKey: 'tabsStack' },
 
-    DRAWER_NOTIFICATIONS: { path: '/drawer/notifications', navKey: 'drawerStack' },
-    DRAWER_CHAT: { path: '/drawer/chat/:user', navKey: 'drawerStack' },
+      DRAWER_NOTIFICATIONS: {
+        path: '/drawer/notifications',
+        navKey: 'drawerStack'
+      },
+      DRAWER_CHAT: { path: '/drawer/chat/:user', navKey: 'drawerStack' },
 
-    STACK_CHAT: { path: '/stack/chat/:user', navKey: 'mainStack' },
+      STACK_CHAT: { path: '/stack/chat/:user', navKey: 'mainStack' },
 
-    KEMA: '/tabs/recent/kem/:user',
-    JAMES: '/tabs/recent/jam/:user'
-  }, {
-    navigators: reduxNavigation({
-      drawer: Drawer,
-      drawerStack: DrawerStack,
-      tabs: Tabs,
-      tabsStack: TabsStack,
-      mainStack: Stack
-    })
-  })
+      KEMA: '/tabs/recent/kem/:user',
+      JAMES: '/tabs/recent/jam/:user'
+    },
+    {
+      navigators: reduxNavigation({
+        drawer: Drawer,
+        drawerStack: DrawerStack,
+        tabs: Tabs,
+        tabsStack: TabsStack,
+        mainStack: Stack
+      })
+    }
+  )
 
   const middlewares = applyMiddleware(thunk, middleware)
   const reducers = createReducer({ location: reducer }, initialPath)
